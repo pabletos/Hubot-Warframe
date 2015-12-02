b71db5eb72fd5529be060d5f90ad744f0ea0870e
nspacestd <nspacestd@users.noreply.github.com>
2015-12-02 19:20:24 +0100

    Add is_active() method to the Library class

--- library.py
+++ library.py
@@ -5,14 +5,20 @@ class Library:
     """
 
     def __init__(self, data):
-        self.target          = data['CurrentTarget']['EnemyType']
-        self.scans           = data['CurrentTarget']['PersonalScansRequired']
-        self.progress        = data['CurrentTarget']['ProgressPercent']
+        if 'CurrentTarget' in data:
+            self.target          = data['CurrentTarget']['EnemyType']
+            self.scans           = data['CurrentTarget']['PersonalScansRequired']
+            self.progress        = data['CurrentTarget']['ProgressPercent']
+            self.active = True
+        else:
+            self.active = False
 
     def __str__(self):
         """Returns a string with all the information about this alert
 
         """
+        if not self.is_active():
+            return None
 
         library_string = ('Target: {0}\n'
                        'Scans needed: {1}\n'
@@ -21,3 +27,9 @@ class Library:
 
         return library_string.format(self.target, self.scans,
                                   self.progress)
+
+    def is_active(self):
+        """ Returns True if there is a currently active target, False otherwise
+
+        """
+        return self.active
